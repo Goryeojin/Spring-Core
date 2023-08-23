995d06f46476f786a93b01886fc4fc79e151ff04
GoRyeoJin <sooon9254@naver.com>
2023-08-23 22:09:42 +0900

    PropertyEditor 를 사용한 데이터 바인딩 추상화

---
+++ demospring51/src/main/java/me/whiteship/demospring51/databinding/Event.java
@@ -0,0 +1,21 @@
+package me.whiteship.demospring51.databinding;
+
+public class Event {
+    private Integer id;
+    private String title;
+    public Event(Integer id) {
+        this.id = id;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
